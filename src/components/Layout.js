a97bcfbd64cdccaac147293d9edfc73244e90ddb
Ron Valstar <ron@ronvalstar.nl>
2020-08-15 09:37:01 +0200

    layout max width

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,10 @@ import {color, css} from '../style'
 
 export const Layout = styled.div`
   width: 61.875rem;
+  max-width: 90vw;
   min-height: calc(100% - 2rem);
   margin: 1rem auto;
   background-color: ${color.white};
   border-radius: ${css.borderRadius};
   box-shadow: 0.5rem 0.5rem 2rem rgba(0,0,0,0.3);
-  //overflow: hidden;
 `
